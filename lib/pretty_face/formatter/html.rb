aa103789e2b049546da7e3ea1b5755ae57464bbd
Joel Byler <joelbyler@gmail.com>
2012-10-31 17:31:02 -0400

    add some simple stats to the report

--- lib/pretty_face/formatter/html.rb
+++ lib/pretty_face/formatter/html.rb
@@ -7,19 +7,49 @@ module PrettyFace
     class Html
       include Cucumber::Formatter::Io
 
+      attr_reader :features, :scenario_count, :step_count, :scenario_average, :step_average
+
       def initialize(step_mother, path_or_io, options)
         @path = path_or_io
         @io = ensure_io(path_or_io, 'html')
         @step_mother = step_mother
         @options = options
+        @scenario_count = 0
+        @step_times = []
+        @scenario_times = []
+      end
+
+      def before_feature_element(feature_element)
+        @scenario_timer = Time.now
+      end
+
+      def after_feature_element(feature_element)
+        @scenario_times.push Time.now - @scenario_timer
+        @scenario_average = @scenario_times.inject{ |sum, el| sum + el }.to_f / @scenario_times .size 
+        @scenario_count = @scenario_times.size
+      end 
+
+      def before_step(step)
+        @step_timer = Time.now
+      end
+
+      def after_step(step)
+        @step_times.push Time.now - @step_timer
+        @step_average = @step_times.inject{ |sum, el| sum + el }.to_f / @step_times .size 
+
+        # This will show hours, minutes, seconds, but when the avg is under a second, it just shows 00:00:00
+        # @step_average = Time.at(@step_times.inject{ |sum, el| sum + el }.to_f / @step_times .size ).gmtime.strftime('%R:%S')
+
+        @step_count = @step_times.size
       end
 
       def after_features(features)
+        @features = features
         images_directory
         filename = File.join(File.dirname(__FILE__), '..', 'templates', 'main.erb')
         text = File.new(filename).read
         renderer = ERB.new(text, nil, "%")
-        @io.puts renderer.result
+        @io.puts renderer.result(binding)
       end
 
       private
