4758c9d73787763513012d17ece8c49182662738
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-11-02 07:33:24 -0400

    reorganized the specs

--- spec/lib/html_formatter_spec.rb
+++ spec/lib/html_formatter_spec.rb
@@ -1,64 +1,59 @@
 require 'spec_helper'
 
 describe PrettyFace::Formatter::Html do
-  it "should track number of scenarios" do
+  let(:formatter) { Html.new(nil, nil, nil) }
 
-    formatter = Html.new nil, nil, nil
+  context "when building the header for the main page" do
+    it "should know the start time" do
+      formatter.before_features(nil)
+      formatter.start_time.should eq Time.now.strftime("%a %B %-d, %Y at %H:%M:%S")
+    end
     
-    formatter.before_feature_element(nil)
-    formatter.after_feature_element(nil)
-    formatter.after_feature_element(nil)
-    formatter.after_feature_element(nil)
-	
-  	formatter.scenario_count.should eq 3
-  end
-  it "should track number of steps" do
+    it "should know how long it takes" do
+      formatter.before_features(nil)
 
-    formatter = Html.new nil, nil, nil
+      formatter.after_features(nil)
+      formatter.total_duration.should include '0.0'
+    end
+  end
+
+  context "when building the report for scenarios" do
+    it "should track number of scenarios" do
+      formatter.before_feature_element(nil)
+      formatter.after_feature_element(nil)
+      formatter.after_feature_element(nil)
+      formatter.after_feature_element(nil)
+      
+      formatter.scenario_count.should eq 3
+    end
+
+    it "should track average scenario durations" do
+      formatter.before_feature_element(nil)
+      formatter.after_feature_element(nil)
+      formatter.before_feature_element(nil)
+      formatter.after_feature_element(nil)
+      
+      formatter.scenario_average_duration.should include '0.0'
+    end
+  end
+
+  context "when building the report for steps" do
+    it "should track number of steps" do
+      formatter.before_step(nil)
+      formatter.after_step(nil)
+      formatter.after_step(nil)
+      
+      formatter.step_count.should eq 2
+    end
     
-    formatter.before_step(nil)
-    formatter.after_step(nil)
-    formatter.after_step(nil)
-	
-  	formatter.step_count.should eq 2
+    it "should track average step durations" do
+      formatter.before_step(nil)
+      formatter.after_step(nil)
+
+      formatter.before_step(nil)
+      formatter.after_step(nil)
+      
+      formatter.step_average_duration.should include '0.0'
+    end
   end
-  it "should track average scenario durations" do
-
-    formatter = Html.new nil, nil, nil
-    
-    formatter.before_feature_element(nil)
-    formatter.after_feature_element(nil)
-
-    formatter.before_feature_element(nil)
-    formatter.after_feature_element(nil)
-	
-  	formatter.scenario_average_duration.should include '0.0'
-  end
-  it "should track average step durations" do
-
-    formatter = Html.new nil, nil, nil
-    
-    formatter.before_step(nil)
-    formatter.after_step(nil)
-
-    formatter.before_step(nil)
-    formatter.after_step(nil)
-	
-  	formatter.step_average_duration.should include '0.0'
-  end
-  it "should know the start time" do
-    formatter = Html.new nil, nil, nil
-    formatter.before_features(nil)
-    formatter.start_time.should eq Time.now.strftime("%a %B %-d, %Y at %H:%M:%S")
-  end
-  it "should know how long it takes" do
-    formatter = Html.new nil, nil, nil
-
-    formatter.before_features(nil)
-
-    formatter.after_features(nil)
-    formatter.total_duration.should include '0.0'
-  end
-
-
 end
