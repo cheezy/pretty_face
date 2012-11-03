19af8ee4cb798bbd2892b948bb6dc7dfb54a3b75
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-11-03 18:30:10 -0400

    another scenario

--- fixtures/basic.feature
+++ fixtures/basic.feature
@@ -2,4 +2,10 @@ Feature: cucumber spike
 
   Scenario: Cucumber puts a string
     When Cucumber puts "hello"
+    Then it should say hello
+
+  Scenario:  A failing scenario
+    When the first step fails
+    Then the second step should not execute
+
 
--- fixtures/step_definitions/basic_steps.rb
+++ fixtures/step_definitions/basic_steps.rb
@@ -1,3 +1,15 @@
 When /^Cucumber puts "(.*?)"$/ do |some_string|
   puts some_string
 end
+
+Then /^it should say hello$/ do
+  puts "It said hello"
+end
+
+When /^the first step fails$/ do
+ true.should == false
+end
+
+Then /^the second step should not execute$/ do
+  puts "Should not execute"
+end
