56f9b51776513183c6b148d7558fb6d635f38a2b
Steve Jackson <steve.jackson@leandogsoftware.com>
2013-01-11 00:38:17 -0500

    Create results directory in hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,9 @@
 require 'fileutils'
 
+Before do
+  FileUtils.mkdir('results') unless File.exists? 'results'
+end
+
 After do
   file = File.join(File.dirname(__FILE__), '..', '..', 'fixture.html')
  # FileUtils.rm file
